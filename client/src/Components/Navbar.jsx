caeca516a7caf4c62a30dad2eb9a29c8e5954e62
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-01-09 21:18:10 +0100

    Still Cooking

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -6,6 +6,9 @@ import { Logo2, menu, search, thirdweb } from '../Assets/Index';
 import {  navlinks } from "../Constants/Index"
 
 const Navbar = () => {
+  const navigate = useNavigate();
+  const [isActive, setIsActive] = useState('dashboard');
+  const [toggleDrawer, setToggleDrawer] = useState(false);
   return (
     <div>Navbar</div>
   )
