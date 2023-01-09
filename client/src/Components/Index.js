78e0ec31b94229ddc339ae6765fb7cb5c26d76a9
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-01-09 21:55:57 +0100

    Still Cooking

--- client/src/Components/Index.js
+++ client/src/Components/Index.js
@@ -1,3 +1,4 @@
 export { default as CustomButton } from "./CustomButton"
+export { default as Loader } from "./Loader"
 export { default as Navbar } from "./Navbar"
 export { default as Sidebar } from "./Sidebar"
--- client/src/Views/CreateCampaign.jsx
+++ client/src/Views/CreateCampaign.jsx
@@ -1,6 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+import { ethers } from 'ethers';
+
+import { money } from '../Assets/Index';
+import { CustomButton } from "../Components/Index"
+import { checkIfImage } from "../Utilities/Index"
 
 const CreateCampaign = () => {
+  const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(false);
+  const { createCampaign } = useStateContext();
+  const [form, setForm] = useState({
+    name: '',
+    title: '',
+    description: '',
+    target: '', 
+    deadline: '',
+    image: ''
+  });
+
+  const handleFormFieldChange = (fieldName, e) => {
+    setForm({ ...form, [fieldName]: e.target.value })
+  }
+  
   return (
     <div>CreateCampaign</div>
   )
