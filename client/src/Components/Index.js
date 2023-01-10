bb5bb57acb30db11010351a1276b3590657095ee
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-01-10 19:59:56 +0100

    Still Cooking

--- client/src/Components/Index.js
+++ client/src/Components/Index.js
@@ -1,4 +1,6 @@
+export { default as CountBox } from "./CountBox"
 export { default as CustomButton } from "./CustomButton"
+export { default as FormField } from "./FormField"
 export { default as Loader } from "./Loader"
 export { default as Navbar } from "./Navbar"
 export { default as Sidebar } from "./Sidebar"
