6e44d45b9cba953b31746d0409924e5ecba35aa3
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-01-11 17:28:58 +0100

    Still Cooking

--- client/src/Context/Index.jsx
+++ client/src/Context/Index.jsx
@@ -1,17 +1,97 @@
 import React, { useContext, createContext } from "react"
-import { useAddress, useContract, useMetamask, useContractWrite }  from "@thirdweb-dev/react"
+import { useAddress, useContract, useMetamask, useContractWrite } from "@thirdweb-dev/react"
 import { ethers } from "ethers"
 import { EditionMetadataWithOwnerOutputSchema } from '@thirdweb-dev/sdk'
 
 const StateContext = createContext()
 
 export const StateContextProvider = ({ children }) => {
-const { contract } = useContract("0x9d7fd648689Df40bb5D19264A043B2c22DbF76EF")
-const { mutateAsync: createCampaign } = useContractWrite(contract, "createCampaigns")
+    const { contract } = useContract("0x9d7fd648689Df40bb5D19264A043B2c22DbF76EF")
+    const { mutateAsync: createCampaign } = useContractWrite(contract, 'createCampaign')
 
-const addresss = useAddress()
-const connect = useMetamask()
+    const address = useAddress()
+    const connect = useMetamask()
+
+    const publishCampaign = async (form) => {
+        try {
+            const data = await createCampaign([
+                address, // owner
+                form.title, // title
+                form.description, // description
+                form.target,
+                new Date(form.deadline).getTime(), // deadline,
+                form.image
+            ])
+
+            console.log("Contracy call was succesful", data)
+        } catch (error) {
+            console.log("Contract call was unsuccessful", error)
+        }
+    }
+
+    const getCampaigns = async () => {
+        const campaigns = await contract.call('getCampaigns')
+
+        const parsedCampaings = campaigns.map((campaign, i) => ({
+            owner: campaign.owner,
+            title: campaign.title,
+            description: campaign.description,
+            target: ethers.utils.formatEther(campaign.target.toString()),
+            deadline: campaign.deadline.toNumber(),
+            amountCollected: ethers.utils.formatEther(campaign.amountCollected.toString()),
+            image: campaign.image,
+            pId: i
+        }))
+
+        return parsedCampaings
+    }
+
+    const getUserCampaigns = async () => {
+        const allCampaigns = await getCampaigns()
+
+        const filteredCampaigns = allCampaigns.filter((campaign) => campaign.owner === address)
+
+        return filteredCampaigns
+    }
+
+    const donate = async (pId, amount) => {
+        const data = await contract.call('donateToCampaign', pId, { value: ethers.utils.parseEther(amount) })
+
+        return data
+    }
+
+    const getDonations = async (pId) => {
+        const donations = await contract.call('getDonators', pId)
+        const numberOfDonations = donations[0].length
+
+        const parsedDonations = []
+
+        for (let i = 0; i < numberOfDonations; i++) {
+            parsedDonations.push({
+                donator: donations[0][i],
+                donation: ethers.utils.formatEther(donations[1][i].toString())
+            })
+        }
+
+        return parsedDonations
+    }
+
+    return (
+        <StateContext.Provider 
+        value={{
+            address,
+            contract,
+            connect,
+            createCampaign: publishCampaign,
+            donate,
+            getCampaigns,
+            getDonations,
+            getUserCampaigns
+        }}>
+            {children}
+        </StateContext.Provider>
+    )
 
 }
 
-export const useStateContext = useStateContext(StateContext)
+export const useStateContext = () => useContext(StateContext)
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router } from 'react-router-dom';
 import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
 
+import { StateContextProvider } from "./Context/Index";
 import App from "./App";
 import "./Styles/Index.css"
 
@@ -12,7 +13,9 @@ root.render(
   <React.StrictMode>
     <ThirdwebProvider desiredChainId={ChainId.Goerli}>
       <Router>
-        <App />
+        <StateContextProvider>
+          <App />
+        </StateContextProvider>
       </Router>
     </ThirdwebProvider>
   </React.StrictMode>
